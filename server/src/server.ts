5fc4aae834f193a957b38d1f4099fa7ad78fc80c
williamscodigo <willliamscodigo@gmail.com>
2025-01-06 14:15:59 -0500

    remove path and filename for static files - render

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,4 @@
 import express from 'express';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import type { Request, Response } from 'express';
 import db from './config/connection.js'
 import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/server-express
@@ -8,9 +6,6 @@ import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './schemas/index.js';
 import { authenticateToken } from './utils/auth.js';
 
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const server = new ApolloServer({
   typeDefs,
@@ -34,10 +29,10 @@ const startApolloServer = async () => {
   ));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static('../client/dist'));
 
     app.get('*', (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile('../client/dist/index.html');
     });
   }
 
