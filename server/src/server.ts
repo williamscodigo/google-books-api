b1bff2ca57ae61652b86a898ac3890c59c54bcef
williamscodigo <willliamscodigo@gmail.com>
2025-01-06 13:48:43 -0500

    modify server.js to pass render - static filename path error

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import type { Request, Response } from 'express';
 import db from './config/connection.js'
 import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/server-express
 import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './schemas/index.js';
 import { authenticateToken } from './utils/auth.js';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server = new ApolloServer({
   typeDefs,
